e14b8b00bc94686977062b7d7cae69fa232ca037
Rob T <Robert.Tootill98@gmail.com>
2021-04-12 12:27:46 +0100

    added demo test case for checking vehicle data

--- test/server.js
+++ test/server.js
@@ -16,55 +16,43 @@ const basic = testData.users[2];
 // %%% functions %%%
 
 /**
- * Searches for jsons with specific information
+ * Compares two vehicle data jsons
+ * @param {[json]} expected 
+ * @param {[json]} real 
+ * @returns validity, error if failed to meet a condition
  */
-function findJsons(vehicles, spec) {
-    const jsons = [];
-    for(const vehicle of vehicles) {
-        for(const key of Object.keys(spec)) {
-            if(vehicle[key] == spec[key]) {
-                jsons.push(vehicle);
-            }
-        }
-    }
-    return jsons;
-}
-
 function checkVehicleDataJSONs(expected, real) {
     if(Object.keys(expected).length != Object.keys(real).length) {
-        return false;
+        return new Error("Outer key lengths do not match");
     }
 
     if(Object.keys(expected.entrance).length != Object.keys(real.entrance).length) {
-        return false;
+        return new Error(`Expected to have ${Object.keys(expected.entrances).length} entrances, got ${Object.keys(real.entrance).length}`);
     }
     for(const key of Object.keys(expected.entrance)) {
         if(expected.entrance[key] != real.entrance[key]) {
-            return false;
+            return new Error(`Expected entrance ${key} to have ${expected.entrance[key]} vehicles, got ${real.entrance[key]}`);
         }
     }
 
     if(Object.keys(expected.exit).length != Object.keys(real.exit).length) {
-        return false;
+        return new Error(`Expected to have ${Object.keys(expected.exit).length} exits, got ${Object.keys(real.exit).length}`);
     }
     for(const key of Object.keys(expected.exit)) {
         if(expected.exit[key] != real.exit[key]) {
-            return false;
+            return new Error(`Expected exit ${key} to have ${expected.exit[key]} vehicles, got ${real.exit[key]}`);
         }
     }
 
     if(Object.keys(expected.route).length != Object.keys(real.route).length) {
-        return false;
+        return new Error(`Expected to have ${Object.keys(expected.route).length} routes, got ${Object.keys(real.route).length}`);
     }
     for(const key of Object.keys(expected.route)) {
         if(expected.route[key] != real.route[key]) {
-            return false;
+            return new Error(`Expected route ${key} to have ${expected.route[key]} vehicles, got ${real.route[key]}`);
         }
     }
-
-    return true;
 }
-
 
 let testSession;
 
@@ -295,40 +283,6 @@ describe("GET /api/vehicle/", () => {
             it("Returns status 200 on access attempt", (done) => {
                 currentSession.get('/api/vehicle')
                     .expect(200)
-                    .expect('Content-Type', /json/)
-                    .expect((res) => {
-                        const expected = {
-                            entrance: {
-                                id0: 1, 
-                                id1: 0, 
-                                id2: 0, 
-                                id3: 0
-                            },
-                            exit: {
-                                id0: 0, 
-                                id1: 1, 
-                                id2: 0, 
-                                id3: 0
-                            },
-                            route: { 
-                                'id0-id1': 1,
-                                'id0-id2': 0,
-                                'id0-id3': 0,
-                                'id1-id0': 0,
-                                'id1-id2': 0,
-                                'id1-id3': 0,
-                                'id2-id0': 0,
-                                'id2-id1': 0,
-                                'id2-id3': 0,
-                                'id3-id0': 0,
-                                'id3-id1': 0,
-                                'id3-id2': 0
-                            }
-                        }
-                        if(!checkVehicleDataJSONs(expected, res.body)) {
-                            throw new Error("Expected vehicle data did not match received vehicle data");
-                        }
-                    })
                     .end((err) => {
                         if(err) {
                             return done(err);
@@ -393,6 +347,41 @@ describe("GET /api/vehicle/", () => {
             it("Received data matches expected data", (done) => {
                 currentSession.get('/api/vehicle')
                     .expect(200)
+                    .expect('Content-Type', /json/)
+                    .expect((res) => {
+                        const expected = {
+                            entrance: {
+                                id0: 3, 
+                                id1: 4, 
+                                id2: 1, 
+                                id3: 3
+                            },
+                            exit: {
+                                id0: 3, 
+                                id1: 3, 
+                                id2: 4, 
+                                id3: 1
+                            },
+                            route: { 
+                                'id0-id1': 2,
+                                'id0-id2': 0,
+                                'id0-id3': 1,
+                                'id1-id0': 1,
+                                'id1-id2': 3,
+                                'id1-id3': 0,
+                                'id2-id0': 0,
+                                'id2-id1': 1,
+                                'id2-id3': 0,
+                                'id3-id0': 2,
+                                'id3-id1': 0,
+                                'id3-id2': 1
+                            }
+                        }
+                        const validity = checkVehicleDataJSONs(expected, res.body);
+                        if(validity instanceof Error) {
+                            throw validity;
+                        }
+                    })
                     .end((err) => {
                         if(err) {
                             return done(err);
