e2751fcad1f65a0ae3959f7e701fc27b2c7303b3
Rob T <Robert.Tootill98@gmail.com>
2021-04-12 11:55:05 +0100

    created test data for test cases npm scripts now exit upon completion

--- server/db/init.js
+++ server/db/init.js
@@ -8,26 +8,27 @@ const users = require('../users.json');
 // connect to database
 const client = new MongoClient(`mongodb://${config.db.domain}:${config.db.port}`, { useUnifiedTopology: true });
 
-client.connect(err => {
+client.connect(async (err) => {
     if (err) {
         console.log(err);
     } else {
         console.log("Setting up database...");
 
         console.log("Connected to MongoDB server");
-        const db = client.db(config.db.name);
+        const db = await client.db(config.db.name);
 
         // if database exists then drop it
-        db.dropDatabase();
+        await db.dropDatabase();
 
         // populate with real data
-        db.collection('users').insertMany(users.users);
+        await db.collection('users').insertMany(users.users);
         // strip password out of json
         for(let i = 0; i < users.users_headless.length; i++) {
             delete users.users_headless[i].password;
         }
-        db.collection('users').insertMany(users.users_headless);
+        await db.collection('users').insertMany(users.users_headless);
 
-        console.log("Database initialised, exit using ctrl+c");
+        console.log("Completed database setup");
     }
+    process.exit();
 });
--- server/db/populate.js
+++ server/db/populate.js
@@ -9,23 +9,24 @@ const testData = require('./testData');
 const client = new MongoClient(`mongodb://${config.db.domain}:${config.db.port}`,
     {useUnifiedTopology: true});
 
-client.connect(err => {
+client.connect(async(err) => {
     if(err) {
         console.log(err);
     } else {
         console.log("Connected to MongoDB server");
-        const db = client.db(config.db.name);
+        const db = await client.db(config.db.name);
 
         // vehicle records
-        db.collection('vehicles').insertMany(testData.vehicles);
+        await db.collection('vehicles').insertMany(testData.vehicles);
     
         // user records
         // strip password out of json
         for(let i = 0; i < testData.users.length; i++) {
             delete testData.users[i].password;
         }
-        db.collection('users').insertMany(testData.users);
+        await db.collection('users').insertMany(testData.users);
 
-        console.log("Database populated, exit using ctrl+c");
+        console.log("Database populated");
     }
+    process.exit();
 });
