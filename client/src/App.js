c8225dc62d922b2b79df404d1a7d86f76cdf7910
stoguri <up781439@myport.ac.uk>
2021-04-19 16:38:44 +0100

    add loading component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react';
 import { makeStyles } from '@material-ui/core/styles'
-import { Box } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import HomeView from './Views/HomeView'
 import LoginView from './Views/LoginView'
 
@@ -16,12 +16,28 @@ const useStyles = makeStyles({
         maxHeight: "100vh",
         maxWidth: "100vw",
         overflowX: 'hidden'
+    },
+    loading: {
+        display: 'flex',
+        height: '100vh',
+        justifyContent: 'center'
     }
 })
 
 export default function App() {
 
+    const { isLoading } = useAuth0();
     const classes = useStyles()
+
+    if (isLoading) {
+        return (
+            <Box className={classes.loading} alignItems='center'>
+                <Typography variant='h1' component='h1'>
+                    loading...
+                </Typography>
+            </Box>
+        )
+    }
     return (
         <Box className={classes.wrapper}>
             <LoginView />
